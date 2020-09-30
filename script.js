6e8aaa89634368e92729fc99de3db301637321c4
deepthijerin <71921082+deepthijerin@users.noreply.github.com>
2020-09-30 21:38:37 +1000

    addedd the links of farenheit and celsius

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function showCity(response) {
-    console.log(response.data);
+   // console.log(response.data);
     document.querySelector("h3").innerHTML = response.data.name;
     document.querySelector("h1").innerHTML = Math.round(response.data.main.temp);
+    variable=Math.round(response.data.main.temp);
     document.querySelector(
       "#goat"
     ).innerHTML = `Humidity:${response.data.main.humidity}%`;
@@ -11,10 +12,34 @@ function showCity(response) {
     document.querySelector("#nature").innerHTML=response.data.weather[0].description;
     let iconElement=document.querySelector("#icon");
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-
+ 
+ 
   }
   
-  function searchCity(city) {
+  function changeFaren(event){
+    event.preventDefault();
+  let h1=document.querySelector("h1");
+   
+  h1.innerHTML=Math.round(((variable * 9/5) + 32));
+  
+}
+function changeCelsius(event){
+    event.preventDefault();
+    let h1=document.querySelector("h1");
+    h1.innerHTML=variable;
+
+}
+
+
+
+let search1=document.querySelector("a#workspace");
+search1.addEventListener("click",changeFaren);
+
+let serach2=document.querySelector("a#workarea");
+serach2.addEventListener("click",changeCelsius);
+  
+let variable=null;
+function searchCity(city) {
     let apiKey = "0942ec9e5ec8ffc97891eab6978cd978";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
     axios.get(`${apiUrl}&appid=${apiKey}`).then(showCity);
@@ -53,20 +78,5 @@ function showCity(response) {
   let search = document.querySelector("form");
   
   search.addEventListener("submit", func1);
-  
-  function func2(event) {
-    event.preventDefault();
-    let h3 = document.querySelector("h1");
-    h3.innerHTML = 60;
-  }
-  let search1 = document.querySelector("a#workspace");
-  
-  search1.addEventListener("click", func2);
-  function func3(event) {
-    event.preventDefault();
-    let h4 = document.querySelector("h1");
-    h4.innerHTML = 8;
-  }
-  let search2 = document.querySelector("a#workarea");
-  search2.addEventListener("click", func3);
+
   
